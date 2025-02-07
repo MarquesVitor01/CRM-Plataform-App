25f8373cb46bc87dede1ae776e7800f1f8098ec6
MarquesVitor01 <marque.vitor47@gmail.com>
2025-02-07 15:52:07 -0300

    um commit

--- Client/src/app/pages/Relatório/Relatorio.tsx
+++ Client/src/app/pages/Relatório/Relatorio.tsx
@@ -31,9 +31,10 @@ export const Relatorio: React.FC = () => {
   >({});
   const [totalBase, setTotalBase] = useState(0);
   const [totalRenovacao, setTotalRenovacao] = useState(0);
-
+  const [totalRecorencia, setTotalRecorencia] = useState(0);
   const [currentPageBase, setCurrentPageBase] = useState(1);
   const [currentPageRenovacao, setCurrentPageRenovacao] = useState(1);
+  const [currentPageRecorencia, setCurrentPageRecorencia] = useState(1);
   const itemsPerPage = 4;
 
   const db = getFirestore();
@@ -47,8 +48,9 @@ export const Relatorio: React.FC = () => {
 
       usuariosSnapshot.forEach((doc) => {
         const usuario = doc.data() as Usuario;
+        // Mantendo o nome como está no banco, sem alteração
         usuarios[doc.id] = usuario;
-        nomeParaID[usuario.nome] = doc.id;
+        nomeParaID[usuario.nome.trim()] = doc.id; // Apenas removendo espaços extras
       });
 
       setUsuariosMap(usuarios);
@@ -74,12 +76,14 @@ export const Relatorio: React.FC = () => {
         // Reset totals
         let baseCount = 0;
         let renovacaoCount = 0;
+        let recorenciaCount = 0;
 
         for (const venda of vendasMapeadas) {
           if (venda.data === today) {
+            // Verificando o operador
             const usuarioID = usuariosPorNome[venda.operador] || venda.operador;
-            const usuario = usuariosMap[usuarioID];
 
+            const usuario = usuariosMap[usuarioID];
             if (usuario) {
               const vendaComAvatar = { ...venda, avatar: usuario.avatar };
               vendasHoje.push(vendaComAvatar);
@@ -94,15 +98,15 @@ export const Relatorio: React.FC = () => {
               baseCount += 1;
             } else if (venda.contrato === "Renovacao") {
               renovacaoCount += 1;
-            }
-            else if (venda.contrato === "Recorencia") {
-              baseCount += 1;
+            } else if (venda.contrato === "Recorencia") {
+              recorenciaCount += 1;
             }
           }
         }
 
         setTotalBase(baseCount);
         setTotalRenovacao(renovacaoCount);
+        setTotalRecorencia(recorenciaCount);
         setVendasDiarias(vendasHoje);
       });
 
@@ -112,19 +116,21 @@ export const Relatorio: React.FC = () => {
     if (Object.keys(usuariosMap).length > 0) {
       fetchVendasDiarias();
     }
-  }, [db, usuariosMap]);
+  }, [db, usuariosMap, usuariosPorNome]);
 
   const vendasPorOperador = vendasDiarias.reduce<
-    Record<string, { base: number; renovacao: number }>
+    Record<string, { base: number; renovacao: number; recorencia: number }>
   >((acc, venda) => {
     const { operador, contrato } = venda;
     if (!acc[operador]) {
-      acc[operador] = { base: 0, renovacao: 0 };
+      acc[operador] = { base: 0, renovacao: 0, recorencia: 0 };
     }
-    if (contrato === "Base" || contrato === "Recorencia") {
+    if (contrato === "Base") {
       acc[operador].base += 1;
     } else if (contrato === "Renovacao") {
       acc[operador].renovacao += 1;
+    } else if (contrato === "Recorencia") {
+      acc[operador].recorencia += 1;
     }
     return acc;
   }, {});
@@ -134,30 +140,40 @@ export const Relatorio: React.FC = () => {
       .filter(
         (operador) =>
           vendasPorOperador[operador].base > 0 ||
+          vendasPorOperador[operador].recorencia > 0 ||
           vendasPorOperador[operador].renovacao > 0
       )
       .sort((a, b) => {
-        const totalA = vendasPorOperador[a].base + vendasPorOperador[a].renovacao;
-        const totalB = vendasPorOperador[b].base + vendasPorOperador[b].renovacao;
+        const totalA =
+          vendasPorOperador[a].base + vendasPorOperador[a].renovacao + vendasPorOperador[a].recorencia;
+        const totalB =
+          vendasPorOperador[b].base + vendasPorOperador[b].renovacao + vendasPorOperador[b].recorencia;
         return totalB - totalA; // Ordenar do maior para o menor
       });
   };
-  
+
   const getTopThree = (tipo: "Base" | "Renovacao" | "Recorencia") => {
     const filtrados = Object.keys(vendasPorOperador).filter((operador) => {
-      if (tipo === "Base" || tipo === "Recorencia") {
+      if (tipo === "Base") {
         return vendasPorOperador[operador].base > 0;
       } else if (tipo === "Renovacao") {
         return vendasPorOperador[operador].renovacao > 0;
+      } else if (tipo === "Recorencia") {
+        return vendasPorOperador[operador].recorencia > 0;
       }
       return false;
     });
-    
-  
+
     return filtrados
       .sort((a, b) => {
-        const totalA = vendasPorOperador[a].base + vendasPorOperador[a].renovacao;
-        const totalB = vendasPorOperador[b].base + vendasPorOperador[b].renovacao;
+        const totalA =
+          vendasPorOperador[a].base +
+          vendasPorOperador[a].renovacao +
+          vendasPorOperador[a].recorencia;
+        const totalB =
+          vendasPorOperador[b].base +
+          vendasPorOperador[b].renovacao +
+          vendasPorOperador[b].recorencia;
         return totalB - totalA; // Ordenar do maior para o menor
       })
       .slice(0, 3);
@@ -165,6 +181,7 @@ export const Relatorio: React.FC = () => {
 
   const topRenovacao = getTopThree("Renovacao");
   const topBase = getTopThree("Base");
+  const topRecorencia = getTopThree("Recorencia");
   const allOperators = getAllOperators();
 
   // Cálculo da paginação para Base
@@ -184,6 +201,7 @@ export const Relatorio: React.FC = () => {
   const indexOfLastRenovacaoOperator = currentPageRenovacao * itemsPerPage;
   const indexOfFirstRenovacaoOperator =
     indexOfLastRenovacaoOperator - itemsPerPage;
+
   const currentRenovacaoOperators = allOperators
     .filter((operador) => vendasPorOperador[operador].renovacao > 0)
     .slice(indexOfFirstRenovacaoOperator, indexOfLastRenovacaoOperator);
@@ -192,6 +210,22 @@ export const Relatorio: React.FC = () => {
       (operador) => vendasPorOperador[operador].renovacao > 0
     ).length / itemsPerPage
   );
+
+  const indexOfLastRecorenciaOperator = currentPageRecorencia * itemsPerPage;
+  const indexOfFirstRecorenciaOperator =
+    indexOfLastRecorenciaOperator - itemsPerPage;
+
+  const currentRecorenciaOperators = allOperators
+    .filter((operador) => vendasPorOperador[operador].recorencia > 0)
+    .slice(indexOfFirstRecorenciaOperator, indexOfLastRecorenciaOperator);
+
+  const totalRecorenciaPages = Math.ceil(
+    Object.keys(vendasPorOperador).filter(
+      (operador) => vendasPorOperador[operador].recorencia > 0
+    ).length / itemsPerPage
+  );
+
+  console.log(currentRecorenciaOperators);
 
   return (
     <section className="dashboard">
@@ -208,7 +242,9 @@ export const Relatorio: React.FC = () => {
                     alt={`${operador} Avatar`}
                     className="img-podio"
                   />
-                  <span className="operador-podio">{operador.replace(/\./g, " ")}</span>
+                  <span className="operador-podio">
+                    {operador.replace(/\./g, " ")}
+                  </span>
                   <span className="vendas-podio">
                     {vendasPorOperador[operador].renovacao}
                   </span>
@@ -235,7 +271,9 @@ export const Relatorio: React.FC = () => {
                     }}
                     className="foto-ranking"
                   />
-                  <span className="nome-operador">{operador.replace(/\./g, " ")}</span>
+                  <span className="nome-operador">
+                    {operador.replace(/\./g, " ")}
+                  </span>
                   <h2 className="vendas-operador">
                     {vendasPorOperador[operador].renovacao}
                   </h2>
@@ -254,7 +292,6 @@ export const Relatorio: React.FC = () => {
             </button>
             <span>
               {currentPageRenovacao} / {totalRenovacaoPages}
-
             </span>
             <button
               onClick={() =>
@@ -268,7 +305,6 @@ export const Relatorio: React.FC = () => {
             </button>
           </div>
         </div>
-
         <div className="ranking base">
           <h4 className="total-contrato">Total Base: {totalBase}</h4>
           <div className="podio">
@@ -281,7 +317,9 @@ export const Relatorio: React.FC = () => {
                     alt={`${operador} Avatar`}
                     className="img-podio"
                   />
-                  <span className="operador-podio">{operador.replace(/\./g, " ")}</span>
+                  <span className="operador-podio">
+                    {operador.replace(/\./g, " ")}
+                  </span>
                   <span className="vendas-podio">
                     {vendasPorOperador[operador].base}
                   </span>
@@ -308,7 +346,9 @@ export const Relatorio: React.FC = () => {
                     }}
                     className="foto-ranking"
                   />
-                  <span className="nome-operador">{operador.replace(/\./g, " ")}</span>
+                  <span className="nome-operador">
+                    {operador.replace(/\./g, " ")}
+                  </span>
                   <h2 className="vendas-operador">
                     {vendasPorOperador[operador].base}
                   </h2>
@@ -338,8 +378,87 @@ export const Relatorio: React.FC = () => {
             </button>
           </div>
         </div>
+        <div className="ranking base">
+          <h4 className="total-contrato">
+            Total Recorrência: {totalRecorencia}
+          </h4>
+          <div className="podio">
+            {topRecorencia.map((operador, index) => {
+              const avatar = usuariosMap[usuariosPorNome[operador]]?.avatar;
+              return (
+                <div key={operador} className={`colocacao${index + 1}`}>
+                  <img
+                    src={avatar}
+                    alt={`${operador} Avatar`}
+                    className="img-podio"
+                  />
+                  <span className="operador-podio">
+                    {operador.replace(/\./g, " ")}
+                  </span>
+                  <span className="vendas-podio">
+                    {vendasPorOperador[operador].recorencia}
+                  </span>
+                </div>
+              );
+            })}
+          </div>
+          <ul className="tabela-ranking text-center">
+            {currentRecorenciaOperators.map((operador) => {
+              const avatar = usuariosMap[usuariosPorNome[operador]]?.avatar;
+              return (
+                <li
+                  key={operador}
+                  style={{ display: "flex", alignItems: "center" }}
+                >
+                  <img
+                    src={avatar}
+                    alt={`${operador} Avatar`}
+                    style={{
+                      width: "50px",
+                      height: "50px",
+                      borderRadius: "50%",
+                      marginRight: "10px",
+                    }}
+                    className="foto-ranking"
+                  />
+                  <span className="nome-operador">
+                    {operador.replace(/\./g, " ")}
+                  </span>
+                  <h2 className="vendas-operador">
+                    {vendasPorOperador[operador].recorencia}
+                  </h2>
+                </li>
+              );
+            })}
+          </ul>
+          <div className="paginacao">
+            <button
+              onClick={() =>
+                setCurrentPageRecorencia((prev) => Math.max(prev - 1, 1))
+              }
+              disabled={currentPageRecorencia === 1}
+            >
+              <FontAwesomeIcon icon={faLeftLong} />
+            </button>
+            <span>
+              {currentPageRecorencia} / {totalRecorenciaPages}
+            </span>
+            <button
+              onClick={() =>
+                setCurrentPageRecorencia((prev) =>
+                  Math.min(prev + 1, totalRecorenciaPages)
+                )
+              }
+              disabled={currentPageRecorencia === totalRecorenciaPages}
+            >
+              <FontAwesomeIcon icon={faRightLong} />
+            </button>
+          </div>
+        </div>
         <div className="total-vendas">
-          <h4>Total de vendas: {totalBase + totalRenovacao}</h4>
+          <h4>
+            Total de vendas: {totalBase + totalRenovacao + totalRecorencia}
+          </h4>
         </div>
       </div>
     </section>
