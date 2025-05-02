9acb10b8ef87953359b8cbc9c3106279df323515
Guilherme <guissilval005@gmail.com>
2025-05-02 09:42:36 -0300

    alterando a rota do cors

--- Server/index.js
+++ Server/index.js
@@ -16,8 +16,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  // origin: process.env.FRONTEND_URL || "https://grupomapscartaodigital.com.br",
-  origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  origin: process.env.FRONTEND_URL || "https://grupomapscartaodigital.com.br",
+  // origin: process.env.FRONTEND_URL || "http://localhost:3000",
 }));
 app.use(bodyParser.json());
 app.use(express.json());
@@ -49,6 +49,6 @@ app.use("/buscar_cnpj", buscarCnpj);
 module.exports = app;
 
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server running on http://localhost:${port}`);
+// });
